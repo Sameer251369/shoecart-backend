cc641ef4af50d2f0414583b2889571734c18c166
Sameer251369 <sameerbashir522@gmail.com>
2025-12-26 19:09:00 +0530

    Fix product image serializer for Cloudinary URLs

--- apps/products/serializers.py
+++ apps/products/serializers.py
@@ -1,10 +1,12 @@
 from rest_framework import serializers
 from .models import Product, ProductImage, Category
 
+
 class CategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = Category
         fields = ['id', 'name', 'slug']
+
 
 class ProductImageSerializer(serializers.ModelSerializer):
     image = serializers.SerializerMethodField()
@@ -14,14 +16,12 @@ class ProductImageSerializer(serializers.ModelSerializer):
         fields = ['id', 'image', 'alt_text']
 
     def get_image(self, obj):
-        if obj.image:
-            return f"http://127.0.0.1:8000{obj.image.url}"
-        return None
+        return obj.image.url if obj.image else None
+
 
 class ProductSerializer(serializers.ModelSerializer):
     images = ProductImageSerializer(many=True, read_only=True)
-    # This allows the frontend to see the category name/slug easily
-    category = CategorySerializer(read_only=True) 
+    category = CategorySerializer(read_only=True)
 
     class Meta:
         model = Product
