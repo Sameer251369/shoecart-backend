8759fc26fda64a8d7d45706c9843630e3a232116
Sameer251369 <sameerbashir522@gmail.com>
2025-12-26 18:43:55 +0530

    Configure Cloudinary media storage for production

--- config/settings.py
+++ config/settings.py
@@ -174,10 +174,3 @@ SIMPLE_JWT = {
 }
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
-
-CLOUDINARY_STORAGE = {
-    'CLOUD_NAME': os.environ.get('CLOUDINARY_CLOUD_NAME'),
-    'API_KEY': os.environ.get('CLOUDINARY_API_KEY'),
-    'API_SECRET': os.environ.get('CLOUDINARY_API_SECRET'),
-}
